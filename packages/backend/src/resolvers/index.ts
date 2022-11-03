0df8af092cdeee43b5a4489f69509191a3ca2ea7
John Paul Ma <jpaulma23@gmail.com>
2022-11-03 14:12:27 -0400

    Tabs 111/tasks backend (#69)
    * Made tasks backend 
    * Finished testing
    
    Co-authored-by: hpnrep6 <hpnrep6@gmail.com>
    Co-authored-by: Gigi <106707569+gcheang@users.noreply.github.com>
    Co-authored-by: crystal huang <35664551+cdecry@users.noreply.github.com>

--- packages/backend/src/resolvers/index.ts
+++ packages/backend/src/resolvers/index.ts
@@ -4,5 +4,7 @@ import category from './categoryResolver'
 import item from './itemResolver'
 import debt from './debtResolver'
 import bill from './billResolver'
+import taskList from './taskListResolver'
+import task from './taskResolver'
 
-export default [user,inventory,category,item,debt,bill]
+export default [user,inventory,category,item,debt,bill,task,taskList]
