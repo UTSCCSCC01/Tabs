108f4f91ddc1a3c69638017f01fa70ceed86ef0e
hpnrep6 <hpnrep6@gmail.com>
2022-11-04 20:53:19 -0400

    Tabs-105: Edit appliances page (#87)
    * Tabs-106: Appliances page
    
    * TABS-114: Fix navigation home page
    
    * TABS-114: Fix navigation home page
    
    * TABS-116 Added Popups for Appliance and Tasks Page
    
    - Added addAppliancePopup.tsx
    - Added addTaskPopup.tsx
    
    * TABS-105: Edit appliances admin page
    
    * TABS-105: Edit appliances page
    
    * TABS-105: Edit appliances page
    
    Co-authored-by: Tyler3412 <62577262+Tyler3412@users.noreply.github.com>

---
+++ packages/frontend/src/data/AdminUtils.tsx
@@ -0,0 +1,10 @@
+/**
+ * Determine whether the user is an admin
+ * 
+ * @name isAdmin
+ * @param userId 
+ * @returns Whether the user is an admin
+ */
+export const isAdmin = (userId: string): boolean => {
+    return true
+}
