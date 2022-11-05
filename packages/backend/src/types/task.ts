43346235c5c9caed1ea1927d344ef17369c678ef
KleinKodes <kleinharrigan@gmail.com>
2022-11-04 23:58:54 -0400

    TABS-108/Completed Issue.

--- packages/backend/src/types/task.ts
+++ packages/backend/src/types/task.ts
@@ -3,7 +3,7 @@ import { Document,  Model } from 'mongoose'
 export interface TaskDocument extends Document {
     houseId: String,
     taskListId:String,
-    author: String,
+    owner: String,
     task: String,
     dateDue: String,
     doneStatus: Boolean
