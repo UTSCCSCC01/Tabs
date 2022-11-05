bd6c969ed4bec413c30f2e2ffbf36a15cb252770
John Paul Ma <jpaulma23@gmail.com>
2022-11-04 23:59:41 -0400

    Tabs 117/show edit category popup on list (#92)

--- packages/frontend/src/ui/fragments/view/FolderListItem.tsx
+++ packages/frontend/src/ui/fragments/view/FolderListItem.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { FolderItemData } from './FolderItemData';
 import { styles } from './mainViewStyles';
 import { BorderIcon } from './BorderIcon';
 import { InventoryItem } from './InventoryItem';
 import { InventoryCategory } from './InventoryCategory';
-
+import EditCategoryPopup from '../../pages/view/editCategoryPopup';
 //i explained this
+
+
 export const FolderListItem = ({item}:{item:FolderItemData}) => {
   
   console.log("LOADING FOLDER LIST ITEM")
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [itemState, setItemState] = useState(item)
+  const onShowPopup = () => {
+    setModalVisible(true)
+  }
+
+  const onClosePopup:(item:InventoryCategory) => void = (item:InventoryCategory) => {
+    setItemState(item)
+    setModalVisible(false)
+  }
 
   if (item instanceof InventoryItem){
     console.log("Will soon say hey listen!");
@@ -42,13 +56,12 @@ export const FolderListItem = ({item}:{item:FolderItemData}) => {
   }
 
   else if (item instanceof InventoryCategory){
+    
     console.log("not gonna say hey listen ever");
     console.log(item.name)
     console.log(item.touchFunction.argument)
     return (
       <TouchableOpacity style={{flex:1, flexGrow:1}} onPress={() => {item.touchFunction.myFunction(item.id)}}>
-  
-  
        
         <View style={styles.folderListItem}>
           {item.iconName != "none" && <BorderIcon iconName={item.iconName}></BorderIcon>}
@@ -56,10 +69,16 @@ export const FolderListItem = ({item}:{item:FolderItemData}) => {
   
   
           <Text style={styles.splitTextNormal}>{item.name}</Text>
+
+
+          <MaterialCommunityIcons  name='pencil' size={30} onPress={onShowPopup}>
           
-  
-  
+          </MaterialCommunityIcons>
+          <EditCategoryPopup itemState={item} show={modalVisible} userId = 'testId' categoryName={item.categoryName} categoryDesc={item.description} categoryId={item.id} isRestricted={item.isRestricted} closePopup={onClosePopup}/>
+
         </View>
+
+        
       </TouchableOpacity>
   
     );
@@ -73,8 +92,6 @@ export const FolderListItem = ({item}:{item:FolderItemData}) => {
        
         <View style={styles.folderListItem}>
           {item.iconName != "none" && <BorderIcon iconName={item.iconName}></BorderIcon>}
-  
-  
   
           <Text style={styles.splitTextNormal}>{item.name}</Text>
           
