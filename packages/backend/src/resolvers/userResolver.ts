6e6818e3f063c02704339394ac76c84aec3cde7e
Gigi <gigi_cheang@hotmail.com>
2022-11-12 20:23:17 -0500

    Add user backend signup and signin

--- packages/backend/src/resolvers/userResolver.ts
+++ packages/backend/src/resolvers/userResolver.ts
@@ -5,7 +5,7 @@ const resolvers = {
     Query: {
         me: async(
             root,
-            args: {username:string},
+            args: {username: String},
             ):
             Promise<UserDocument | null> =>{
                 return await User.findById(args).exec()
@@ -15,12 +15,35 @@ const resolvers = {
     Mutation: {
         signUp: async(
             root,
-            args: {email: string; password:string}
+            args: {email: String, username: String, password: String, phone: String}
         ): Promise<UserDocument> =>{
 
             const user = await User.create(args)
-            console.log("added to server")
+            console.log("Account added to server")
             return user
+        },
+
+        signIn: async(
+            root,
+            args: {username: String, password: String}
+        ): Promise<UserDocument> =>{
+            const user = await User.findOne({ username: args.username }).select("password")
+            if (user == null) {
+                console.log("User not found")
+            } else if (await user.matchesPassword(args.password)) {
+                console.log("Password matches")
+            } else {
+                console.log("Password does not match")
+            }
+            return user
+        },
+
+        signOut: async(
+            root,
+            args: {}
+        ): Promise<Boolean> =>{
+            console.log("signOut called")
+            return true
         }
     }
 }
