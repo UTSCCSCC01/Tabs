5435c1a3e1cfa466f53a3805bf83e72fa3765a24
crystal / ue <mfihy1@gmail.com>
2022-11-17 23:27:36 -0500

    TABS-127: Add navigation for back arrow

--- packages/frontend/src/ui/fragments/view/common/HeaderComponent.tsx
+++ packages/frontend/src/ui/fragments/view/common/HeaderComponent.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 export type Props = {
     screenName: string;
@@ -9,14 +11,15 @@ export type Props = {
 const HeaderComponent: React.FC<Props> = ({
     screenName
 }) => {
+    var navigation = useNavigation();
   return (
     <View style={styles.myHeader}>
 
         <View style={styles.rowFlex2}>
-
-        <MaterialCommunityIcons name="arrow-left" size={24} style={styles.headerIcon}/>
-
-        <Text style={styles.myHeaderText}>{screenName}</Text>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={styles.headerIcon}>
+                <MaterialCommunityIcons name="arrow-left" size={24}/>
+            </TouchableOpacity>
+            <Text style={styles.myHeaderText}>{screenName}</Text>
 
         </View>
 
@@ -33,25 +36,24 @@ const styles = StyleSheet.create({
       },
     
       myHeaderText: {
-        textAlign: 'left',
+        flex: 10,
+        textAlign: 'center',
         fontWeight: "bold",
         fontSize: 18,
         fontFamily: "Arial",
-        width: "66%",
       },
 
       rowFlex2:{
+        display: 'flex',
         flex: 1,
         flexDirection: "row",
-        justifyContent: "space-evenly",
         height:"100%",
         width:"100%",
-    
         alignItems: 'center',
       },
 
       headerIcon: {
-        width: "33%",
+        flex: 1,
        },
     
 
